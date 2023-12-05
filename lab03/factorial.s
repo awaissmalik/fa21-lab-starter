a4ac2efe7f9b64f761ed73daa2d1c0437b80a982
awaissmalik <140106268+awaissmalik@users.noreply.github.com>
2023-12-05 17:30:47 +0500

    Update factorial.s

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -21,4 +21,23 @@ main:
     ecall # Exit
 
 factorial:
-    # YOUR CODE HERE
+    # Recursive factorial function
+    # Input: a0 - n
+    # Output: a0 - n!
+
+    # Base case: n = 0 or n = 1
+    beqz a0, base_case
+    li t1, 1
+    beq a0, t1, base_case
+
+    # Recursive case: n! = n * (n-1)!
+    subi a0, a0, 1         # Decrement n
+    jal ra, factorial      # Recursive call
+    lw a1, 0(a0)            # Load the result of (n-1)!
+    mul a0, a0, a1         # Multiply n by (n-1)!
+
+    ret
+
+base_case:
+    li a0, 1               # Base case: n! = 1
+    ret
